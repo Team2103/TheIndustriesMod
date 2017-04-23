666f8065bd5893340a7990e359a6dd146c2739e9
alesharik <alesharikReserv@yandex.ru>
2017-04-23 15:49:36 +0300

    Repo cleanup

--- src/main/java/com/maks2103/industries/GuiResearchBook.java
+++ src/main/java/com/maks2103/industries/GuiResearchBook.java
@@ -1,11 +1,6 @@
 package com.maks2103.industries;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 
 public class GuiResearchBook extends GuiContainer {
     public ContainerResearchBook container;
@@ -21,7 +16,7 @@ public class GuiResearchBook extends GuiContainer {
 //        GlStateManager.translate((width - xSize) / 2, (height - ySize) / 2, 0);
 //        Minecraft.getMinecraft().getRenderItem().renderItem(new ItemStack(Items.APPLE), ItemCameraTransforms.TransformType.GUI);
 //        GlStateManager.popMatrix();
-        this.mc.getTextureManager().bindTexture(GUI_TEXTURE);
+//        this.mc.getTextureManager().bindTexture(GUI_TEXTURE);
         drawTexturedModalRect((width - xSize) / 2, (height - ySize) / 2, 0, 0, xSize, ySize);
     }
 }
