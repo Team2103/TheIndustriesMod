04587306db3dede4373385dd4526caaacfe17237
alesharik <alesharikReserv@yandex.ru>
2017-04-28 21:29:34 +0300

    Added RemoteCaller

---
+++ src/main/java/com/maks2103/industries/net/CallRemoteMethodMessage.java
@@ -0,0 +1,151 @@
+package com.maks2103.industries.net;
+
+import com.maks2103.industries.util.serialization.NBTSerializable;
+import com.maks2103.industries.util.serialization.NBTSerializableSerializer;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+/**
+ * DO NOT USE IT
+ */
+public final class CallRemoteMethodMessage implements IMessage {
+    private static final int LIST_TAG_TYPE;
+
+    static {
+        NBTTagList list = new NBTTagList();
+        list.appendTag(NBTSerializableSerializer.serialize(new NBTSerializable() {
+            @Nonnull
+            @Override
+            public NBTTagCompound serializeNBT() {
+                return new NBTTagCompound();
+            }
+
+            @Override
+            public void deserializeNBT(@Nonnull NBTTagCompound compound) {
+
+            }
+        }));
+        LIST_TAG_TYPE = list.getTagType();
+    }
+
+    private List<Object> params;
+    private String methodName;
+    private String className;
+
+    CallRemoteMethodMessage(List<Object> params, String methodName, String className) {
+        this.params = params;
+        this.methodName = methodName;
+        this.className = className;
+    }
+
+    public CallRemoteMethodMessage() {
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        deserializeNBT(ByteBufUtils.readTag(buf));
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        ByteBufUtils.writeTag(buf, serializeNBT());
+    }
+
+    private void deserializeNBT(NBTTagCompound compound) {
+        this.params = new ArrayList<>();
+
+        NBTTagList params = compound.getTagList("params", LIST_TAG_TYPE);
+        for(int i = 0; i < params.tagCount(); i++) {
+            this.params.add(NBTSerializableSerializer.deserialize(params.getCompoundTagAt(i)));
+        }
+        methodName = compound.getString("methodName");
+        className = compound.getString("className");
+    }
+
+    @SuppressWarnings("RedundantCast")
+    private NBTTagCompound serializeNBT() {
+        NBTTagCompound compound = new NBTTagCompound();
+        compound.setString("methodName", methodName);
+        compound.setString("className", className);
+        NBTTagList list = new NBTTagList();
+        params.forEach(o -> list.appendTag(NBTSerializableSerializer.serialize((Object & NBTSerializable) o)));
+        compound.setTag("params", list);
+        return compound;
+    }
+
+    public static final class Handler implements IMessageHandler<CallRemoteMethodMessage, IMessage> {
+
+        @Override
+        public IMessage onMessage(CallRemoteMethodMessage message, MessageContext ctx) {
+            MinecraftServer server = ctx.getServerHandler().playerEntity.getServer();
+            if(server == null) {
+                System.err.println("WTF!");
+                return null;
+            }
+            server.addScheduledTask(() -> processMessage(message, ctx));
+            return null;
+        }
+
+        private void processMessage(CallRemoteMethodMessage message, MessageContext ctx) {
+            try {
+                Class<?> clazz = Class.forName(message.className);
+                Object[] params = message.params.toArray();
+                Class<?>[] classes = Stream.of(params)
+                        .map(Object::getClass)
+                        .toArray((IntFunction<Class<?>[]>) Class[]::new);
+                Stream.of(clazz.getDeclaredMethods())
+                        .filter(method -> method.getName().equals(message.methodName))
+                        .filter(m -> {
+                            try {
+                                int retClassCounter = 0;
+                                for(Class<?> aClass : m.getParameterTypes()) {
+                                    if(!(MessageContext.class.isAssignableFrom(aClass) || classes[retClassCounter++].isAssignableFrom(aClass))) {
+                                        return false;
+                                    }
+                                }
+                                return true;
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                e.printStackTrace();
+                                return false;
+                            }
+                        })
+                        .findAny()
+                        .ifPresent(m -> {
+                            List<Object> ret = new ArrayList<>();
+                            int retClassCounter = 0;
+                            for(Class<?> aClass : m.getParameterTypes()) {
+                                if(MessageContext.class.isAssignableFrom(aClass)) {
+                                    ret.add(ctx);
+                                } else if(classes[retClassCounter].isAssignableFrom(aClass)) {
+                                    ret.add(params[retClassCounter++]);
+                                } else {
+                                    ret.add(null);
+                                }
+                            }
+                            Object[] methodParams = ret.toArray();
+                            m.setAccessible(true);
+                            try {
+                                m.invoke(null, methodParams);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/maks2103/industries/net/RemoteCaller.java
@@ -0,0 +1,52 @@
+package com.maks2103.industries.net;
+
+import com.maks2103.industries.IndustriesMod;
+import com.maks2103.industries.util.serialization.NBTSerializable;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import scala.actors.threadpool.Arrays;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.List;
+
+/**
+ * If method has {@link net.minecraftforge.fml.common.network.simpleimpl.MessageContext}, then current message context
+ * will be passed into argument
+ */
+public final class RemoteCaller {
+    private RemoteCaller() {
+    }
+
+    public static <T extends Object & NBTSerializable> void callRemote(@Nonnull Method method, @Nonnull Side side, T... params) {
+        CallRemoteMethodMessage message = createMessage(method, params);
+        if(side.isClient()) {
+            IndustriesMod.getNetworkWrapper().sendToServer(message);
+        } else {
+            IndustriesMod.getNetworkWrapper().sendToAll(message);
+        }
+    }
+
+    public static <T extends Object & NBTSerializable> void callOnClient(@Nonnull Method method, @Nonnull EntityPlayerMP player, T... params) {
+        IndustriesMod.getNetworkWrapper().sendTo(createMessage(method, params), player);
+    }
+
+    public static <T extends Object & NBTSerializable> void callOnAllClientAround(@Nonnull Method method, @Nonnull NetworkRegistry.TargetPoint point, T... params) {
+        IndustriesMod.getNetworkWrapper().sendToAllAround(createMessage(method, params), point);
+    }
+
+    public static <T extends Object & NBTSerializable> void callOnDimensionClients(@Nonnull Method method, int dimId, T... params) {
+        IndustriesMod.getNetworkWrapper().sendToDimension(createMessage(method, params), dimId);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Object & NBTSerializable> CallRemoteMethodMessage createMessage(Method method, T[] params) {
+        if(!Modifier.isStatic(method.getModifiers())) {
+            throw new RuntimeException("Method must be static!");
+        }
+        List<Object> paramList = Arrays.asList(params);
+        return new CallRemoteMethodMessage(paramList, method.getName(), method.getDeclaringClass().getName());
+    }
+}
